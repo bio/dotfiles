c37c0bf25041c6d1abbe68613bc143110085f9e8
Valery Viktorovsky <viktorovsky@gmail.com>
2025-11-27 02:50:11 +0200

    Delete duplicate indexes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,14 +1,14 @@
 local wezterm = require 'wezterm'
 
-local TITLEBAR_BG_COLOR <const> = "#f0f0f0"
+local TITLEBAR_BG_COLOR <const> = '#f0f0f0'
 
 return {
   -- text
   font = wezterm.font('Monaco'),
   font_size = 16,
   -- iTerm2 style with anti-aliasing disabled
-  freetype_load_target = "Mono",
-  freetype_load_flags = "NO_AUTOHINT",
+  freetype_load_target = 'Mono',
+  freetype_load_flags = 'NO_AUTOHINT',
   line_height = 0.98,
   cell_width = 1.04,
 
@@ -19,24 +19,24 @@ return {
   use_fancy_tab_bar = true,
   show_close_tab_button_in_tabs = false,
   show_new_tab_button_in_tab_bar = false,
-  window_decorations = "NONE",
+  window_decorations = 'NONE',
   initial_cols = 171,
   initial_rows = 50,
   window_frame = {
     -- The font used in the tab bar
     font = wezterm.font({
-      family = "Roboto",
-      weight = "Bold",
+      family = 'Roboto',
+      weight = 'Bold',
     }),
     font_size = 11.0,
     active_titlebar_bg = TITLEBAR_BG_COLOR,
     inactive_titlebar_bg = TITLEBAR_BG_COLOR,
   },
   window_padding = {
-    left = "0.8cell",
-    right = "0.8cell",
-    top = "0.2cell",
-    bottom = "0.3cell",
+    left = '0.8cell',
+    right = '0.8cell',
+    top = '0.2cell',
+    bottom = '0.3cell',
   },
 
   keys = {
@@ -49,39 +49,35 @@ return {
   },
 
   colors = {
-    foreground = "#000000",
-    background = "#ffffff",
+    foreground = '#000000',
+    background = '#ffffff',
 
-    ansi = {"black", "maroon", "green", "olive", "#4285f4", "purple", "teal", "silver"},
-    brights = {"grey", "red", "lime", "yellow", "#4285f4", "fuchsia", "aqua", "white"},
+    ansi = {'black', 'maroon', 'green', 'olive', '#4285f4', 'purple', 'teal', 'silver'},
+    brights = {'grey', 'red', 'lime', 'yellow', '#4285f4', 'fuchsia', 'aqua', 'white'},
 
-    cursor_fg = "#ffffff",
-    cursor_bg = "#000000",
-    cursor_border = "#000000",
+    cursor_fg = '#ffffff',
+    cursor_bg = '#000000',
+    cursor_border = '#000000',
 
-    selection_fg = "#ffffff",
-    selection_bg = "#555555",
+    selection_fg = '#ffffff',
+    selection_bg = '#555555',
 
     tab_bar = {
-      background = "#dcdcdc",
-      background = "#ffffff",
-      background = "#e0e0e0",
       background = TITLEBAR_BG_COLOR,
 
       active_tab = {
-        bg_color = "#ede8ed",
-        bg_color = "#ffffff",
-        fg_color = "#000000",
+        bg_color = '#ffffff',
+        fg_color = '#000000',
 
-        -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+        -- Specify whether you want 'Half', 'Normal' or 'Bold' intensity for the
         -- label shown for this tab.
-        -- The default is "Normal"
-        intensity = "Normal",
+        -- The default is 'Normal'
+        intensity = 'Normal',
 
-        -- Specify whether you want "None", "Single" or "Double" underline for
+        -- Specify whether you want 'None', 'Single' or 'Double' underline for
         -- label shown for this tab.
-        -- The default is "None"
-        underline = "None",
+        -- The default is 'None'
+        underline = 'None',
 
         -- Specify whether you want the text to be italic (true) or not (false)
         -- for this tab.  The default is false.
@@ -93,23 +89,23 @@ return {
       },
 
       inactive_tab = {
-        bg_color = "#cccccc",
-        fg_color = "#999999",
-        intensity = "Half",
+        bg_color = '#cccccc',
+        fg_color = '#999999',
+        intensity = 'Half',
       },
       inactive_tab_hover = {
-        bg_color = "#cccccc",
-        fg_color = "#999999",
+        bg_color = '#cccccc',
+        fg_color = '#999999',
       },
 
       -- The new tab button that let you create new tabs
       new_tab = {
         bg_color = TITLEBAR_BG_COLOR,
-        fg_color = "#999999",
+        fg_color = '#999999',
       },
       new_tab_hover = {
         bg_color = TITLEBAR_BG_COLOR,
-        fg_color = "#000000",
+        fg_color = '#000000',
       },
     },
   },
